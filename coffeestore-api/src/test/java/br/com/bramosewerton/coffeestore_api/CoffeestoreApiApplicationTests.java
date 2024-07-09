fb237a01abbb90d8341ae15a902863c329262beb
Ewerton Bramos <ewertonmatheus2113@gmail.com>
2024-07-09 00:22:22 -0300

    Adicionando back end

---
+++ coffeestore-api/src/test/java/br/com/bramosewerton/coffeestore_api/CoffeestoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bramosewerton.coffeestore_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeestoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
